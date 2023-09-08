26743a2b1554dd00c702a8d9d220764962694fa8
dburnat <d.burnat97@gmail.com>
2023-09-08 15:17:54 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.89d3ac91ab6aeff5164d7ee69c379b0e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7791869894c2dec5831a825ff165cd97",
+    "url": "/part-placer/649eb75d772d9c025db7.worker.js"
+  },
+  {
+    "revision": "058d78496ef29b8736fc8149925dd2c4",
+    "url": "/part-placer/index.html"
+  },
+  {
+    "revision": "3c184d307c71eb766f1f",
+    "url": "/part-placer/static/js/2.0dd6db78.chunk.js"
+  },
+  {
+    "revision": "3b282228993b49924c68f19b71f6e5e3",
+    "url": "/part-placer/static/js/2.0dd6db78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81035c6744677c7e33d3",
+    "url": "/part-placer/static/js/3.de4a423c.chunk.js"
+  },
+  {
+    "revision": "f20d260e85c4019fd8aa",
+    "url": "/part-placer/static/js/4.df3c6508.chunk.js"
+  },
+  {
+    "revision": "9f3fc1506660af852c82",
+    "url": "/part-placer/static/js/5.53e12255.chunk.js"
+  },
+  {
+    "revision": "1eeb57e35bcf9e7abb11",
+    "url": "/part-placer/static/js/6.b51a0929.chunk.js"
+  },
+  {
+    "revision": "ae1461a18230f1703c63",
+    "url": "/part-placer/static/js/main.a781f9b5.chunk.js"
+  },
+  {
+    "revision": "6486b043fd671b3b5a5d",
+    "url": "/part-placer/static/js/runtime-main.f0ec11fb.js"
+  }
+]);
